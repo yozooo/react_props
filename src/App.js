684d37b92771254e7aafd614e59219a4203df870
hyperv <hyperv@ucione.local>
2022-03-25 19:55:25 +0100

    React-props

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+// u class prebact
+
+export default class App extends React.Component {
+  
+  
+  render ()
+  {
+    return (
+      <>
+      
+      </>
+    );
+
+  }
+    
+}
+
+
